5f3ffe62330c0c2448698d68b45e0a9ad3b9c530
ajw <anjy0821@naver.com>
2026-01-26 16:39:47 +0900

    판매자 등록 버그 수정

--- src/api/auth.js
+++ src/api/auth.js
@@ -36,6 +36,11 @@ export const authAPI = {
         })
         return response.data;
     },
+    // 토큰 리프레시
+    refresh: async() => {
+        const response = await api.get(`/auth/refresh`)
+        return response.data;
+    },
     // 로그아웃
     logout: async() => {
         const response = await authApi.get(`/auth/logout`)
